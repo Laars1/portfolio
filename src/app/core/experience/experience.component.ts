29cb13e077735c2c292a05e8537bdfce93184485
Lars Bischhausen <lars.bischhausen@gmail.com>
2023-10-26 16:28:55 +0200

    added education

--- src/app/core/experience/experience.component.ts
+++ src/app/core/experience/experience.component.ts
@@ -7,12 +7,51 @@ import { IExperienceItem } from 'src/app/shared/dtos/experience-item';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-  item: IExperienceItem = {
-    jobtitle: "Test",
-    companyName: "Post CH AG",
-    timerange: "2016-2022",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  experienceItems: IExperienceItem[] = [  {
+    jobtitle: "Software-Engineer and Coach ICT Academy",
+    companyName: "Swiss Post",
+    timerange: "August 2020 - present",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
+  },
+  {
+    jobtitle: "Apprenticeship as Information Technologist and in specialism application development",
+    companyName: "Swiss Post",
+    timerange: "August 2016 - July 2020",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
   }
+]
+
+
+educationItems: IExperienceItem[] = [{
+  jobtitle: "Bachelor's degree in Business Information Technology",
+    companyName: "Bern Universityof Applied Sciences",
+    timerange: "September 2021 - July 2025",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
+},
+{
+  jobtitle: "Apprenticeship Educator",
+    companyName: "berufsbildner.ch",
+    timerange: "November 2020",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
+},
+{
+  jobtitle: "Federal Vocational Baccalaureate",
+    companyName: "gibb",
+    timerange: "August 2016 - July 2020",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
+},
+{
+  jobtitle: "Apprenticeship training",
+    companyName: "gibb",
+    timerange: "August 2016 - July 2020",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa",
+    place: "Bern, Switzerland"
+}]
   constructor() { }
 
   ngOnInit() {
